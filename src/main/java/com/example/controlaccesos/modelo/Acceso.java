d021f0e832acb661bac5e0aaee9e383f77e596b6
FACULTADES\alumnoFI <alumnoFI>
2025-09-02 20:35:53 -0300

    primeros cambios

--- src/main/java/com/example/controlaccesos/modelo/Acceso.java
+++ src/main/java/com/example/controlaccesos/modelo/Acceso.java
@@ -1,10 +1,49 @@
 package com.example.controlaccesos.modelo;
 
-import java.sql.Timestamp;
-import lombok.Data;
+import java.util.Date;
 
-@Data
+
 public class Acceso {
-    private Timestamp entrada;
-    private Timestamp salida;
+    private Date entrada;
+    private Date salida;
+    
+    public Acceso() {
+    }
+
+    
+
+    public Acceso(Date entrada, Date salida) {
+        this.entrada = entrada;
+        this.salida = salida;
+    }
+
+
+
+    public Date getEntrada() {
+        return entrada;
+    }
+
+    public void setEntrada(Date entrada) {
+        this.entrada = entrada;
+    }
+
+    public Date getSalida() {
+        return salida;
+    }
+
+    public void setSalida(Date salida) {
+        this.salida = salida;
+    }
+
+    public int diffTiempoAcceso(){
+        if(this.entrada == null || this.salida == null || salida.before(entrada)){
+            return 0;
+        }
+
+        int difTiempoMiliseg = (int)(salida.getTime() - entrada.getTime()) ;
+
+        return difTiempoMiliseg / (60*1000);
+    }
+    
+
 }
--- src/main/java/com/example/controlaccesos/modelo/Usuario.java
+++ src/main/java/com/example/controlaccesos/modelo/Usuario.java
@@ -1,13 +1,70 @@
 package com.example.controlaccesos.modelo;
 
-import java.util.List;
-import lombok.Data;
+import java.util.ArrayList;
 
-@Data
+
 public class Usuario {
     private String userName;
     private String nombre;
     private String apellido;
     private boolean habilitado;
-    private List<Acceso> accesos;
+    private ArrayList<Acceso> accesos;
+    
+    public Usuario(String userName, String nombre, String apellido, boolean habilitado) {
+        this.userName = userName;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.habilitado = habilitado;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public boolean isHabilitado() {
+        return habilitado;
+    }
+
+    public void setHabilitado(boolean habilitado) {
+        this.habilitado = habilitado;
+    }
+
+    public ArrayList<Acceso> getAccesos() {
+        return accesos;
+    }
+
+    public void setAccesos(ArrayList<Acceso> accesos) {
+        this.accesos = accesos;
+    }
+
+    public int totalTiempoAccesos(){
+        int tiempoTotal = 0;
+        for (Acceso acceso : accesos) {
+            tiempoTotal += acceso.diffTiempoAcceso();
+        }
+        return tiempoTotal;
+    }
+
+    
+    
 }
